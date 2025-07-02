0795625217a5b73b355f5408feb568939dd5acb5
Job Boronsong <jboronsong@gmail.com>
2025-07-02 21:24:45 +0000

    Update App.js

--- torchbearers-backend/src/App.js
+++ torchbearers-backend/src/App.js
@@ -1,18 +1,14 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  const token = localStorage.getItem('adminToken');
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to={token ? "/dashboard" : "/login"} />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
