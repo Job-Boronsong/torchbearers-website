2ab111a96ce69bcf3b8c0c5f96dd4d20aa58dd49
Job Boronsong <jboronsong@gmail.com>
2025-07-02 19:42:02 +0000

    update spinner

--- torchbearers-backend/src/components/LoadingSpinner.js
+++ torchbearers-backend/src/components/LoadingSpinner.js
@@ -1,13 +1,8 @@
-// src/components/LoadingSpinner.js
 import React from 'react';
 import './LoadingSpinner.css';
 
-const LoadingSpinner = () => {
-  return (
-    <div className="spinner-container">
-      <div className="spinner"></div>
-    </div>
-  );
-};
+const LoadingSpinner = () => (
+  <div className="spinner">Loading...</div>
+);
 
 export default LoadingSpinner;
